8b4272b7f7a3cfd0c539f1ff124056435138955a
darrenkuro <darrenkuro@icloud.com>
2023-05-17 14:34:35 +0200

    update

--- submitted/sort_simple.c
+++ submitted/sort_simple.c
@@ -6,13 +6,17 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:30:08 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/16 22:26:10 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/17 14:28:27 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "ft_printf.h"
-#include "libft.h"
+
+void	sort_two(t_ui *arr, t_list **op)
+{
+	if (arr[1] < arr[0])
+		ft_lstadd_back(op, ft_lstnew("sa"));
+}
 
 /* */
 void	sort_three(t_ui *arr, t_list **op)
